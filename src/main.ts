83cda12e207a0355e6a42151bfe60a765d3eeeff
ByteCodeMonkey <2020214225@sdtbu.edu.cn>
2025-06-27 16:58:05 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter';
-import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./core/filter/http-exception/http-exception.filter";
+import { TransformInterceptor } from "./core/interceptor/transform/transform.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,5 +15,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on:3000`);
+  console.log(process.env.PORT);
 }
 bootstrap();
